c75b54b100577566a7aa4d71af453c8b82b9d2ff
JisuLim <sanola3@gmail.com>
2018-03-11 19:53:58 +0900

    RestController 사용

--- src/main/java/com/example/baseball/controller/RestController.java
+++ src/main/java/com/example/baseball/controller/RestController.java
@@ -1,10 +1,51 @@
 package com.example.baseball.controller;
 
+import com.example.baseball.BaseBallMethod;
+import com.example.baseball.model.GameInfo;
+import com.example.baseball.model.GameInfoRepository;
+import com.example.baseball.validator.GameInfoValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
+@CrossOrigin("*")
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/*")
 public class RestController {
 
+    BaseBallMethod baseBallMethod = new BaseBallMethod();
+
+    @Autowired
+    private GameInfoRepository gameInfoRepository;
+
+    @RequestMapping("/init")
+    public String gameInit(@ModelAttribute("GameInfo") GameInfo gameInfo, BindingResult result) {
+
+        String page = "redirect:/";
+        Date date = new Date();
+
+        gameInfo.setNumber(baseBallMethod.genProblemNumber(gameInfoRepository));
+        gameInfo.setDate(date);
+
+        GameInfoValidator validator = new GameInfoValidator();
+        validator.validate(gameInfo, result);
+
+        if(!result.hasErrors()) {
+            gameInfoRepository.save(gameInfo);
+            page = "play";
+        }
+        return page;
+    }
+
+    @RequestMapping("/test")
+    public String test(Model model) {
+        System.out.println("버튼 동작");
+        return "play";
+    }
 
 }
