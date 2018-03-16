e59c971f12fce209278c47c82aac1409b713990c
JisuLim <sanola3@gmail.com>
2018-03-16 17:19:15 +0900

    정답의 개수 3개 이상으로 구현

--- src/main/java/com/example/baseball/BaseBallMethod.java
+++ src/main/java/com/example/baseball/BaseBallMethod.java
@@ -10,32 +10,32 @@ import java.util.Random;
 public class BaseBallMethod {
 
     //정답이 될 숫자를 생성하는 메소드 (현재는 3자리숫자로 만듦)
-    public String genProblemNumber(GameInfoRepository gameInfoRepository) {
+    public String genProblemNumber(GameInfoRepository gameInfoRepository, int probNum) {
         String result = "";
         Random rand = new Random();
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < probNum; i++) {
             int num = rand.nextInt(8);
             result += Integer.toString(num + 1);
         }
-        result = checkProblemNumber(gameInfoRepository, result);
+        result = checkProblemNumber(gameInfoRepository, result, probNum);
 
         return result;
     }
 
     //이전 게임에서 사용된 정답과 같은 숫자를 생성하지 않기 위해 체크하는 메소드
-    private String checkProblemNumber(GameInfoRepository gameInfoRepository, String problemNumber) {
+    private String checkProblemNumber(GameInfoRepository gameInfoRepository, String problemNumber, int probNum) {
         List<String> problemNumberFromDb = new ArrayList<>();
         problemNumberFromDb = gameInfoRepository.findAllProblemNumbers();
 
         if (problemNumberFromDb.contains(problemNumber))
-            return genProblemNumber(gameInfoRepository);
+            return genProblemNumber(gameInfoRepository, probNum);
 
         return problemNumber;
     }
 
     //정답 체크 int 의 index 0 = strike, 1 = ball, 2 = point
-    public int[] evalAnswer(int problem, String answer) {
+    public int[] evalAnswer(int problem, String answer, int probNum) {
         int strike = 0;
         int ball = 0;
         int[] result = new int[3];
@@ -53,11 +53,10 @@ public class BaseBallMethod {
                 }
             }
         }
-        for(int k=0; k< 3; k++)
         result[0] = strike;
         result[1] = ball;
 
-        if(strike == 3)
+        if(strike == probNum)
             result[2] = 10000;
         else
             result[2] = strike*3 + ball*1;
--- src/main/java/com/example/baseball/controller/RestController.java
+++ src/main/java/com/example/baseball/controller/RestController.java
@@ -28,17 +28,17 @@ public class RestController {
     private GameInfoRepository gameInfoRepository;
 
     @RequestMapping("/init")
-    public ResponseEntity<?> gameInit(@ModelAttribute("GameInfo") GameInfo gameInfo, BindingResult result) {
+    public ResponseEntity<?> gameInit(@ModelAttribute("GameInfo") GameInfo gameInfo, BindingResult result, @RequestParam int probNum) {
 
         Date date = new Date();
 
-        gameInfo.setNumber(baseBallMethod.genProblemNumber(gameInfoRepository));
+        gameInfo.setNumber(baseBallMethod.genProblemNumber(gameInfoRepository, probNum));
         gameInfo.setDate(date);
 
         GameInfoValidator validator = new GameInfoValidator();
         validator.validate(gameInfo, result);
 
-        if(!result.hasErrors()) {
+        if(!result.hasErrors() && probNum >= 3) {
             gameInfoRepository.save(gameInfo);
             ResultCode resultCode = new ResultCode("200");
             resultCode.setGameInfo(gameInfo);
@@ -49,9 +49,9 @@ public class RestController {
     }
 
     @RequestMapping("/answer")
-    public ResponseEntity<?> getAnswer(@RequestParam String answer, @RequestParam int gameIdx) {
+    public ResponseEntity<?> getAnswer(@RequestParam String answer, @RequestParam int gameIdx, @RequestParam int probNum) {
         int[] result;
-        result = baseBallMethod.evalAnswer(gameInfoRepository.findNumberByIdx(gameIdx), answer);
+        result = baseBallMethod.evalAnswer(gameInfoRepository.findNumberByIdx(gameIdx), answer, probNum);
 
         gameInfoRepository.setPointAndTryNum(result[2], gameIdx);
 
